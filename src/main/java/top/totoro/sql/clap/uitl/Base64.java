ccd068fe979e458701de178aed8425a95ecf4085
totoro-dev <2844150336@qq.com>
2020-09-22 11:18:10 +0800

    [Fix][取消加密解密，因为会概率性出现乱码]

--- src/main/java/top/totoro/sql/clap/uitl/Base64.java
+++ src/main/java/top/totoro/sql/clap/uitl/Base64.java
@@ -2,6 +2,7 @@ package top.totoro.sql.clap.uitl;
 
 /**
  * 数据的加密解密
+ * 2020/9/22 取消加密解密，因为会概率性出现乱码
  */
 public class Base64 {
     /**
@@ -11,13 +12,14 @@ public class Base64 {
      * @return 返回解密后的新字符串
      */
     public static String encode(String data) {
-        //把字符串转为字节数组
-        byte[] b = data.getBytes();
-        //遍历
-        for (int i = 0; i < b.length; i++) {
-            b[i] += 1;//在原有的基础上+1
-        }
-        return new String(b);
+//        //把字符串转为字节数组
+//        byte[] b = data.getBytes();
+//        //遍历
+//        for (int i = 0; i < b.length; i++) {
+//            b[i] += 1;//在原有的基础上+1
+//        }
+//        return new String(b);
+        return data;
     }
 
     /**
@@ -27,12 +29,13 @@ public class Base64 {
      * @return 返回解密后的新字符串
      */
     public static String decode(String data) {
-        //把字符串转为字节数组
-        byte[] b = data.getBytes();
-        //遍历
-        for (int i = 0; i < b.length; i++) {
-            b[i] -= 1;//在原有的基础上-1
-        }
-        return new String(b);
+//        //把字符串转为字节数组
+//        byte[] b = data.getBytes();
+//        //遍历
+//        for (int i = 0; i < b.length; i++) {
+//            b[i] -= 1;//在原有的基础上-1
+//        }
+//        return new String(b);
+        return data;
     }
 }
